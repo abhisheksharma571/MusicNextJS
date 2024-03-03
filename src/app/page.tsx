938cfd3b10c87415fee4cd457bd6eb2bcf9c9dcf
Abhishek Sharma <sharmabhishek802158@gmail.com>
2024-03-04 04:27:40 +0530

    added many components but needed some debugging and changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -7,6 +12,11 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
